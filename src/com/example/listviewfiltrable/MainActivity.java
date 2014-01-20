66a52422431cb7d0739169b203c80b16d2d3b7c7
kalnemi <kalpen.vaghela@gmail.com>
2014-01-20 15:45:43 +0530

    ok

---
+++ src/com/example/listviewfiltrable/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.listviewfiltrable;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends Activity 
+{
+	String[] countries = new String[] {
+			"India",
+			"Pakistan",
+			"Sri Lanka",
+			"China",
+			"Bangladesh",
+			"Nepal",
+			"Afghanistan",
+			"North Korea",
+			"South Korea",
+			"Japan"
+	};
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String> ( this, android.R.layout.simple_list_item_1, countries );
+		ListView listView = (ListView) findViewById( R.id.listView ); 
+		listView.setAdapter(adapter);
+		listView.setTextFilterEnabled(true);
+	}
+}
